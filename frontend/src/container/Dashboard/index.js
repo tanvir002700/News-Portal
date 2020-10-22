d93ef95713d67029675648830306bde589a320ea
tanvir <tanvir.hasan@codemarshal.com>
2020-10-22 16:56:06 +0600

    feat(app): add search text box area

--- frontend/src/container/Dashboard/index.js
+++ frontend/src/container/Dashboard/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import { TextField, InputAdornment, Button } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+}));
 
 
 const Dashboard = props => {
+  const classes = useStyles();
+
   return (
-    <h1>Dashboard</h1>
+    <React.Fragment>
+      <h1>Dashboard</h1>
+      <TextField
+        className={classes.margin}
+        id="input-with-icon-textfield"
+        label="TextField"
+        variant="outlined"
+        margin="dense"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button
+        data-testid="submit"
+        type="submit"
+        variant="contained"
+        color="primary">
+          Search
+      </Button>
+    </React.Fragment>
   );
 };
 
