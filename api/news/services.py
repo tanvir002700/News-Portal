7ee0e6af7d9313ba1bf75dfd45cc8213be1959bb
Tanvir Hasan <tanvir002700@gmail.com>
2021-02-06 22:04:28 +0600

    feat(api): handle new api query params exception

--- api/news/services.py
+++ api/news/services.py
@@ -1,5 +1,6 @@
 from newsapi import NewsApiClient
 from django.conf import settings
+from rest_framework import exceptions, status
 
 class NewApiService:
     KEY = getattr(settings, 'NEWS_API_KEY', '')
@@ -11,9 +12,14 @@ class NewApiService:
         country_query = query_params.get('country')
         category_query = query_params.get('category')
         source_query = query_params.get('source')
-        top_headlines = newsapi.get_top_headlines(
-            q=search_query,
-            country=country_query,
-            category=category_query,
-            sources=source_query)
+        try:
+            top_headlines = newsapi.get_top_headlines(
+                q=search_query,
+                country=country_query,
+                category=category_query,
+                sources=source_query)
+        except ValueError as e:
+            raise exceptions.ValidationError(
+                str(e), status.HTTP_400_BAD_REQUEST)
+
         return top_headlines
